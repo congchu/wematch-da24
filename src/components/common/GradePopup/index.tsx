fb513bd524f9b5acdbc8c24cd5eaf508149c20fc
marcus <rhcn258@gmail.com>
2020-09-18 15:06:07 +0900

    평가등급 모달 추가
    1. NewPartner 모달
    2. Partner 등급 모달

---
+++ src/components/common/GradePopup/index.tsx
@@ -0,0 +1,100 @@
+import React from 'react'
+import styled from 'styled-components'
+import ToastPopup from "components/wematch-ui/ToastPopup";
+
+import * as colors from 'styles/colors'
+
+const GradeToastPopupTitle = styled.p`
+  font-size: 24px;
+  font-weight: bold;
+  line-height: 36px;
+  letter-spacing: -0.03em;
+  color: ${colors.gray33};
+  text-align: left;
+`;
+
+const GradeToastPopupSubTitle = styled.div`
+  font-size: 16px;
+  line-height: 24px;
+  letter-spacing: -0.02em;
+  word-break: keep-all;
+  margin-top: 10px;
+  margin-bottom: 20px;
+  font-weight: normal;
+  text-align: left;
+  span {
+    font-weight: bold;
+    color: ${colors.pointVividBlue};
+  }
+`;
+
+const GradeToastPopupTable = styled.table`
+  width: 100%;
+  font-size: 14px;
+  line-height: 24px;
+  letter-spacing: -1px;
+  margin-top: 30px;
+  th {
+    font-weight: bold;
+    padding: 11px;
+    box-sizing: border-box;
+    border-bottom: 1px solid ${colors.gray33};
+    text-align: center;
+    width: 50%;
+  }
+  td {
+    font-weight: normal;
+    padding: 11px;
+    box-sizing: border-box;
+    text-align: center;
+    border-bottom: 1px solid ${colors.lineDefault};
+  }
+`;
+
+interface Props {
+    visible: boolean;
+    onClose: () => void;
+}
+const GradeToastPopup:React.FC<Props> = (props) => {
+    const { visible, onClose } = props;
+    return (
+        <ToastPopup visible={visible} showHeaderCancelButton={true} cancelClick={() => onClose()}>
+            <GradeToastPopupTitle>소비자평가등급이란?</GradeToastPopupTitle>
+            <GradeToastPopupSubTitle>
+                <span>최근 1년간 해당 업체를 이용한 고객의 평가(전문성,친절도,가성비)</span>를 빅데이터로 분석해 산출하는 서비스 등급입니다.
+            </GradeToastPopupSubTitle>
+            <GradeToastPopupTable>
+                <thead>
+                <tr>
+                    <th>등급</th>
+                    <th>만족 확률</th>
+                </tr>
+                </thead>
+                <tbody>
+                 <tr>
+                     <td>S</td>
+                     <td>84%</td>
+                 </tr>
+                 <tr>
+                     <td>A</td>
+                     <td>73%</td>
+                 </tr>
+                 <tr>
+                     <td>B</td>
+                     <td>65%</td>
+                 </tr>
+                 <tr>
+                     <td>C</td>
+                     <td>51%</td>
+                 </tr>
+                 <tr>
+                     <td>등급산정중</td>
+                     <td>74%</td>
+                 </tr>
+                </tbody>
+            </GradeToastPopupTable>
+        </ToastPopup>
+    )
+};
+
+export default GradeToastPopup
---
+++ src/components/common/NewPartner/index.tsx
@@ -0,0 +1,80 @@
+import React, {useState} from 'react';
+import styled from 'styled-components'
+import * as colors from "../../../styles/colors";
+import {Question} from "../../Icon";
+import LevelIcon from "../../LevelIcon";
+import {useMedia} from "react-use-media";
+import GradeToastPopup from "../GradePopup";
+
+const NewPartnerWrapper =  styled.div`
+  display: flex;
+  padding: 24px;
+  background-color: ${colors.boxBg};
+  border-radius: 8px;
+  margin: 35px 0 45px;
+`;
+const GradeInfo = styled.div`
+  min-width: 65px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  span {
+  	display: flex;
+  	color: ${colors.gray66};
+  	font-size: 13px;
+  }
+  
+  svg {
+  	margin-left: 3px;
+  }
+  img {
+  	width: 44px;
+  	padding-top: 7px;
+  }
+`;
+const NewPartnerDescription =  styled.div`
+  margin-left: 25px;
+  text-align: left;
+  p {
+  	font-size: 13px;
+  	font-weight: bold;
+  	color: ${colors.gray66};
+  	margin-bottom: 9px;
+  }
+  div {
+  	font-size: 14px;
+  	color: ${colors.gray33};
+  	line-height: 1.5;
+  }
+  @media screen and (min-width: 768px) {
+  	margin-left: 55px;
+  }
+`;
+
+const NewPartner = () => {
+    const [visible, setVisible] = useState(false)
+    const isMobile = useMedia({
+        maxWidth: 767,
+    })
+    return (
+        <>
+        <NewPartnerWrapper>
+            <GradeInfo onClick={() => setVisible(!visible)}>
+						<span>평가등급
+							<Question width={16} height={16} />
+						</span>
+                <LevelIcon level={'NEW'}/>
+            </GradeInfo>
+            <NewPartnerDescription>
+                <p>위매치 신규 파트너</p>
+                <div>통계적으로 NEW등급의 고객만족 확률은 A등급보다 높습니다.</div>
+                {!isMobile && <div>(S등급 84%, A등급 73%, New등급 74%)</div>}
+            </NewPartnerDescription>
+        </NewPartnerWrapper>
+        <GradeToastPopup visible={visible} onClose={() => setVisible(!visible)}/>
+        </>
+    )
+};
+
+export default NewPartner
