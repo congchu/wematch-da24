5e83005c6d14e5f149641f51424d32ee87ca5032
Ryan <ryan@marketdesigner.com>
2020-11-02 18:02:08 +0900

    배너 이미지 교체, 배너 컴포넌트 반응형 추가 작업

--- src/pages/new/components/PartnerBanner/index.tsx
+++ src/pages/new/components/PartnerBanner/index.tsx
@@ -24,30 +24,72 @@ const Banner = styled.section`
     line-height: 21px;
     letter-spacing: -0.03em;
     color: ${colors.gray66};
-    margin-bottom: 21px;
   }
   .banner {
-    height: 80px;
     position: relative;
-    background: linear-gradient(225deg, ${colors.pointBlue} 0%, ${colors.pointSky} 100%);
+    min-width: 312px;
+    width: 100%;
+    height: 80px;
+    background-color: #f7f7f7;
     border-radius: 8px;
     
-    .text {
+    display: flex;
+    align-items: center;
+    .box {
       font-style: normal;
       font-weight: 500;
       font-size: 14px;
       line-height: 21px;
-      display: flex;
-      align-items: center;
       letter-spacing: -0.03em;
-      color: ${colors.white};
-      padding-top: 18px;
-      padding-left: 20px;
+      color: ${colors.gray33};
+
+      margin-left: 20px;
+
+      .title {
+        font-weight: 500;
+      }
     }
-    img {
+    
+    &:after {
+      content: '';
+      background-image: url(${require('assets/images/partner_banner.svg')});
+      background-repeat: no-repeat;
+      background-size: cover;
+      width: 174px;
+      height: 80px;
       position: absolute;
-      right: 0;
       top: 0;
+      right: 0;
+      border-top-right-radius: 8px;
+      border-bottom-right-radius: 8px;
+    }
+  }
+  
+  @media screen and (min-width:768px) {
+    .banner {
+      width: 100%;
+      height: 120px;
+      
+      .box {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        line-height: 30px;
+        
+        .title {
+          font-weight: bold;
+          font-size: 20px;
+        }
+        .text {
+          font-size: 20px;
+        }
+      }
+      
+      &:after {
+        width: 204px;
+        height: 120px;
+      }
     }
   }
 `
@@ -57,10 +99,12 @@ export default function PartnerBanner() {
         <Banner>
             <a href="https://da24.wematch.com/partnernew.asp">
                 <h3>파트너 등록문의</h3>
-                <p>No.1 이사업체 매칭플랫폼 위매치다이사</p>
+                <p style={{ marginBottom: 21 }}>No.1 이사업체 매칭플랫폼 위매치다이사</p>
                 <div className="banner">
-                    <p className="text">500만 고객의 선택!<br /> 이제 영업은 저희에게 맡기세요</p>
-                    <img src={require('assets/images/partner_banner.svg')} alt="Banner"/>
+                    <div className="box">
+                        <strong className="title">500만 고객의 선택! &nbsp;</strong>
+                        <p className="text">이제 영업은 저희에게 맡기세요</p>
+                    </div>
                 </div>
             </a>
         </Banner>
