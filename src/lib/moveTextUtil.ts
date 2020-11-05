b5ea3655475e7ab5eca67cba0a6918a937f0c19d
sunship <sunship1128@gmail.com>
2020-11-05 17:23:30 +0900

    필요없는 파일 삭제

--- src/lib/moveTextUtil.ts
+++
@@ -1,7 +0,0 @@
-export const getMoveTypeText = (moveType: 'house' | 'oneroom' | 'office' | undefined) => {
-    if (moveType === 'house') {
-        return '가정'
-    } else if (moveType === 'office') {
-        return '사무실'
-    }
-}
