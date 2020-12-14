c4132c239ecee79f6f46eee6e45f07a3da657e3a
Ryan <ryan@marketdesigner.com>
2020-12-14 16:16:54 +0900

    checkApp 수정, 디바이스 아이디 가져오 hook 추가

--- src/hooks/useReceiveMessage.ts
+++ src/hooks/useReceiveMessage.ts
@@ -10,15 +10,17 @@ export default function useReceiveMessage() {
 
     useEffect(() => {
         // ios에서는 window로 가져와야하고 android에서는 document로 가져와야함.
-        if (checkApp() && checkIos()) {
-            window.addEventListener('message', handleMessage, false)
-            return () => {
-                window.removeEventListener('message', handleMessage)
-            }
-        } else {
-            document.addEventListener('message', handleMessage, false)
-            return () => {
-                window.removeEventListener('message', handleMessage)
+        if (checkApp()) {
+            if (checkIos()) {
+                window.addEventListener('message', handleMessage, false)
+                return () => {
+                    window.removeEventListener('message', handleMessage)
+                }
+            } else {
+                document.addEventListener('message', handleMessage, false)
+                return () => {
+                    window.removeEventListener('message', handleMessage)
+                }
             }
         }
     }, [])
