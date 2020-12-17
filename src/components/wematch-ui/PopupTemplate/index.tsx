073bcb6b57566550df4dab09877a4a19e129be24
Yesdoing <yesdoing13@gmail.com>
2020-12-17 14:09:15 +0900

    [refactoing] 팝업 템플릿 스타일 수정

--- src/components/wematch-ui/PopupTemplate/index.tsx
+++ src/components/wematch-ui/PopupTemplate/index.tsx
@@ -1,49 +1,49 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react'
 import styled from 'styled-components'
 import * as colors from "../../../styles/colors";
-import {Icon} from "../index";
+import { Icon } from "../index";
 
 interface Props {
-    visible: boolean;
-    onClose: () => void;
+  visible: boolean;
+  onClose: () => void;
+  pcHeight?: number;
 }
 
-const PopupTemplate:React.FC<Props> = (props) => {
-    const { visible, children, onClose } = props;
-    useEffect(() => {
-        if (visible) {
-            document.body.style.overflow = 'hidden'
-        }
+const PopupTemplate: React.FC<Props> = (props) => {
+  const { visible, children, onClose, pcHeight } = props;
+  useEffect(() => {
+    if (visible) {
+      document.body.style.overflow = 'hidden'
+    }
 
-        return () => document.body.removeAttribute('style')
-    })
+    return () => document.body.removeAttribute('style')
+  }, [visible])
 
-    if (!visible) return null
+  if (!visible) return null
 
-    return (
-        <PopupOverlay>
-            <PopupWrapper>
-                <PopupHeader>
-                    <div onClick={onClose}>
-                        <Icon.Close size={20} color={'#121212'} />
-                    </div>
-                </PopupHeader>
-                {children}
-            </PopupWrapper>
-        </PopupOverlay>
-    )
+  return (
+    <PopupOverlay>
+      <PopupWrapper pcHeight={pcHeight}>
+        <PopupHeader>
+          <div onClick={onClose}>
+            <Icon.Close size={20} color={'#121212'} />
+          </div>
+        </PopupHeader>
+        {children}
+      </PopupWrapper>
+    </PopupOverlay>
+  )
 }
 
-export default  PopupTemplate
+export default PopupTemplate
 
 const PopupOverlay = styled.div`
   position: fixed;
   top: 0;
   left: 0;
   width: 100%;
-  height: 100%;
-  z-index: 10;
-  
+  height: 100vh;
+  z-index: 200;
   background-color: white;
   
   @media screen and (min-width: 768px) {
@@ -51,15 +51,19 @@ const PopupOverlay = styled.div`
   }
 `;
 
-const PopupWrapper = styled.div`
+const PopupWrapper = styled.div<{ pcHeight?: number }>`
   position: relative;
-  height: 100%;
+  height: inherit;
+  display: flex;
+  flex-direction: column;
   overflow-y: auto;
   background-color: white;
-  
+  padding-top: 56px;
+  box-sizing: border-box;
+
   @media screen and (min-width: 768px) {
     width: 360px;
-    height: 480px;
+    height: ${({ pcHeight }) => !pcHeight ? '480px' : `${pcHeight}px`};
     border-radius: 16px;
     top: 50%;
     left: 50%;
@@ -68,12 +72,16 @@ const PopupWrapper = styled.div`
 `;
 
 const PopupHeader = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
   display: flex;
   align-items: center;
   justify-content: flex-end;
   background-color: white;
   height: 56px;
-  
+  z-index: 210;
   svg {
     margin-right: 20px;
   }
