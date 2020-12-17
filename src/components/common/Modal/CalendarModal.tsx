142159cfbf58403ce1d8e497ebf68e6d07738f31
Ryan <ryan@marketdesigner.com>
2020-12-17 15:03:41 +0900

    캘린더 모달창 55일 경고 문구 추

--- src/components/common/Modal/CalendarModal.tsx
+++ src/components/common/Modal/CalendarModal.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import Styled from 'styled-components'
-import dayjs from "dayjs";
+import dayjs from 'dayjs'
 
 import ModalTemplate from './ModalTemplate'
-import DatePicker  from "components/common/DatePicker"
-import { CalendarDate } from "components/wematch-ui/utils/date";
+import DatePicker  from 'components/common/DatePicker'
+import { CalendarDate } from 'components/wematch-ui/utils/date'
 
 interface Props {
     visible: boolean
@@ -25,7 +25,35 @@ const S = {
             margin: 5px 0 10px 0;
             padding: 0;
         }
-    `
+    `,
+    Info: Styled.div`
+      margin-top: 30px;
+      padding: 16px 16px 14px;
+      border: 1px solid #D7DBE2;
+      border-radius: 6px;
+      color: #333333;
+      .infoTitle{
+        position: relative;
+        padding-left: 13px;
+        font-size: 14px;
+        line-height: 21px;
+      .dot {
+        position: absolute;
+        top: 5px;
+        left: -1px;
+        width: 8px;
+        height: 8px;
+        border-radius: 4px;
+        background-color: #F79016;
+      }
+      em {
+        display: inline-block;
+        margin-bottom: 2px;
+        font-size: 15px;
+        font-weight: 600;
+      }
+    }
+  `,
 }
 
 const CalendarModal: React.FC<Props> = (props) => {
@@ -62,6 +90,9 @@ const CalendarModal: React.FC<Props> = (props) => {
             <S.Container>
                 <DatePicker currentDate={new Date()} rangeStartDate={rangeStartDate} rangeEndDate={rangeEndDate}
                     onSelect={onSelect} selected={selected} disabledDate={disabledDate} />
+                <S.Info>
+                    <p className="infoTitle"><em>조회가능 날짜</em><br />내일부터 55일 이내 날짜에만 이사업체 <br /> 조회가 가능해요!</p>
+                </S.Info>
             </S.Container>
         </ModalTemplate>
     )
