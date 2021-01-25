0750a77b5063dfc3df7d493b91a6b2ebb924e023
Chanhee Jeong <chan420420@gmail.com>
2021-01-25 17:57:27 +0900

    .

---
+++ src/pages/company/Detail/components/partnerInfo/index.tsx
@@ -0,0 +1,244 @@
+import React, {useState} from 'react'
+import styled from 'styled-components'
+
+import {useMedia} from "react-use-media";
+import { isEmpty } from 'lodash'
+
+import * as colors from 'styles/colors'
+import * as values from 'constants/values'
+
+import LevelModal from 'components/Modal/LevelModal'
+import LevelIcon from 'components/LevelIcon'
+import NewPartner from 'components/common/NewPartner'
+import { Question } from 'components/Icon'
+import { Level, LevelText } from 'types/partner'
+import UserImage from "./userImage";
+
+const S = {
+	Container: styled.div`
+		position:relative;
+		margin-top:-16px;
+		border-top-left-radius:10px;
+		border-top-right-radius:10px;
+		background:${colors.white};
+		border-bottom:8px solid ${colors.lineDeco};
+		@media screen and (min-width:1200px) {
+			width:720px;
+			margin:0 auto;
+			padding-left:272px;
+			border-bottom:0;
+		}
+	`,
+	Wrap: styled.div`
+		padding:25px 24px;
+		letter-spacing:-0.5px;
+		@media screen and (min-width:768px) {
+			width:608px;
+			margin:0 auto;
+			padding:41px 0 24px;
+		}
+		@media screen and (min-width:1200px) {
+			width:656px;
+			padding-top:38px;
+		}
+	`,
+	LevelDescription: styled.span`
+		display:block;
+		font-size:14px;
+		font-weight:500;
+	`,
+	Level: styled.strong`
+		display:block;
+		margin-top:5px;
+		font-size:20px;
+		font-weight:700;
+		color:${colors.pointBlue}
+	`,
+	PartnerWord: styled.p`
+    display:-webkit-box;
+    overflow:hidden;
+    width:100%;
+    margin-top:10px;
+		font-size:24px;
+    font-weight:700;
+    line-height:30px;
+    text-overflow:ellipsis;
+    -webkit-line-clamp:2;
+    -webkit-box-orient:vertical;
+    word-wrap:break-word;
+	`,
+	Info: styled.div`
+		overflow:visible;
+		height:112px;
+		margin:35px 0 45px;
+		div:first-child{
+			cursor:pointer;
+		}
+		div:last-child{
+			margin-right:0;
+		}
+		@media screen and (min-width:768px) {
+			margin:35px 0 48px;
+		}
+	`,
+	Card: styled.div`
+		float:left;
+		width:32%;
+		height:100%;
+		margin-right:2%;
+		border-radius:8px;
+  	box-shadow:0 3px 20px 0 rgba(220, 220, 220, 0.7);
+ 	 	background-color:${colors.white};
+		text-align:center;
+		span{
+			display:block;
+			padding-top:17px;
+			color:${colors.gray66};
+			font-size:13px;
+			svg{
+				margin-left:4px;
+				vertical-align:top;
+			}
+		}
+		img{
+			width:56px;
+			padding-top:7px;
+		}
+		em{
+			display:inline-block;
+			padding-top:20px;
+			color:${colors.black};
+			font-size:24px;
+			font-weight:700;
+		}
+		@media screen and (min-width:1200px) {
+			width:32.5%;
+			margin-right:1%;
+		}
+	`,
+	Description: styled.div`
+		div:last-child{
+			margin-bottom:-4px;
+		}
+	`,
+	Option: styled.div`
+		margin-bottom:44px;
+		strong{
+			display:block;
+			margin-bottom:12px;
+			font-size:16px;
+			font-weight:700;
+		}
+		p{
+			font-size:16px;
+			color:${colors.gray33};
+			line-height:24px;
+			white-space: pre-line;
+		}
+		ul{
+			overflow:hidden;
+		}
+		li{
+			float:left;
+			margin-right:2%;
+			margin-bottom:6px;
+			padding:10px 14px 8px;
+			border:1px solid ${colors.lineDefault};
+			border-radius:20px;
+			font-size:16px;
+			color:${colors.gray33};
+		}
+		@media screen and (min-width:768px) {
+			margin-bottom:41px;
+		}
+	`,
+	Border: styled.span`
+		display:none;
+		width:720px;
+		border-bottom:8px solid ${colors.lineDeco};
+		@media screen and (min-width:1200px) {
+			display:block;
+		}
+	`,
+}
+
+interface Props {
+	title: string;
+	level: Level
+	pick_cnt: number;
+	experience: number;
+	description: string;
+	keywords: string[];
+	adminname: string;
+	addition?: string;
+	profile_img: string;
+	status: 'selected' | 'available' | 'unavailable';
+}
+
+const Index = ({ title, level, pick_cnt, experience, description='', keywords, adminname, addition='', status, profile_img }: Props) => {
+	const [visibleLevelModal, setVisibleLevelModal] = useState(false)
+	const isMobile = useMedia({
+		maxWidth: 767,
+	})
+
+	const toggleVisibleLevel = () => setVisibleLevelModal(!visibleLevelModal)
+
+
+	return (
+		<>
+			<UserImage profile_img={profile_img} status={status} />
+			<S.Container>
+				<S.Wrap>
+					<S.LevelDescription>{LevelText[level]}</S.LevelDescription>
+					<S.Level>{level === 'NEW' ? '등급산정중' : `고객평가 ${level}등급`}</S.Level>
+					<S.PartnerWord>{title}</S.PartnerWord>
+					{level === 'NEW'
+						? <NewPartner showQuestionIcon={true}/>
+						: <S.Info>
+							<S.Card onClick={toggleVisibleLevel} id="dsl_booking_detail_info">
+						<span>평가등급
+							<Question width={16} height={16} />
+						</span>
+								<LevelIcon level={level}/>
+							</S.Card>
+							<S.Card>
+								<span>고객선택</span>
+								<em>{pick_cnt || 0} 회</em>
+							</S.Card>
+							<S.Card>
+								<span>경력년차</span>
+								<em>{experience || 1} 년</em>
+							</S.Card>
+						</S.Info>
+					}
+					<S.Description>
+						<S.Option>
+							<strong>사장님 한마디({adminname})</strong>
+							<p>{description.length !== 0 ? description : values.DEFAULT_TEXT}</p>
+						</S.Option>
+						{!isEmpty(keywords) &&
+						<S.Option>
+							<strong>고객이 많이 언급한 키워드</strong>
+							<ul>
+								{keywords.map((list, index) => (
+									<li key={index}>{list}</li>
+								))}
+							</ul>
+						</S.Option>
+						}
+						{addition.length > 0 && (
+							<S.Option>
+								<strong>추가 가능 옵션</strong>
+								<p>{addition}</p>
+							</S.Option>
+						)}
+					</S.Description>
+				</S.Wrap>
+				<S.Border />
+				<LevelModal visible={visibleLevelModal} onClose={toggleVisibleLevel} />
+			</S.Container>
+		</>
+	)
+}
+
+export default Index
--- src/pages/company/Detail/index.tsx
+++ src/pages/company/Detail/index.tsx
@@ -1,9 +1,301 @@
-import React from 'react'
+import React, { useEffect, useState, useRef } from 'react'
+import styled, { css } from 'styled-components'
+import { useParams, useHistory } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { useMedia } from 'react-use-media'
+
+import { DownArrow, UpArrow } from 'components/Icon'
+import Loading from 'components/Loading'
+import MainHeader from 'components/MainHeader'
+import TopGnb from 'components/TopGnb'
+import PartnerInfo from './components/partnerInfo/index'
+import ReviewContainer from "./components/reviewContainer/index";
+
+import * as colors from 'styles/colors'
+import * as values from 'constants/values'
+
+import * as partnerActions from 'store/partner/actions'
+import * as partnerSelector from 'store/partner/selectors'
+import * as formSelector from "store/form/selectors";
+import * as commonSelector from 'store/common/selectors'
+import { some } from "lodash";
+import { useRouter } from 'hooks/useRouter'
+import ToastPopup from "components/wematch-ui/ToastPopup";
+import { dataLayer } from "lib/dataLayerUtil";
+import SetType from "components/SetType";
+
+const S = {
+    Container: styled.div``,
+    BottomContainer: styled.div`
+		position:relative;
+		margin-top:10px;
+		:before{
+			content:'';
+			position:absolute;
+			top:-10px;
+			width:100%;
+			height:8px;
+			margin-bottom:64px;
+			background-color:${colors.lineDeco};
+			border:1px solid ${colors.lineDefault};
+			box-sizing:border-box;
+			@media screen and (min-width:1200px) {
+				width:720px;
+				border:0;
+			}
+		}
+		@media screen and (min-width:1200px) {
+			width:720px;
+			margin:0 auto;
+			padding-bottom:80px;
+			padding-left:272px;
+		}
+	`,
+    MoreList: styled.button`
+		width:100%;
+		height:52px;
+		margin-bottom:64px;
+		font-size:15px;
+		font-weight:500;
+		background-color:${colors.white};
+		color:${colors.gray66};
+		cursor:pointer;
+		svg{
+			margin-left:4px;
+			vertical-align:middle;
+		}
+		@media screen and (min-width:1200px) {
+			margin-bottom:0;
+		}
+	`,
+
+
+    BtnSelect: styled.button<{ status: 'selected' | 'available' | 'unavailable', isSelected: boolean }>`
+		position:fixed;
+		z-index:5;
+		left:0;
+		right:0;
+		bottom:0;
+		width:100%;
+		height:64px;
+		font-size:18px;
+		font-weight:700;
+		background-color: ${props => props.isSelected ? colors.lineDefault : colors.pointBlue};
+		color:${colors.white};
+		cursor:pointer;
+		${props => props.status === 'unavailable' && css`
+			background-color:${colors.lineDefault};
+			pointer-events: none;  
+		`};
+		@media screen and (min-width:1200px) {
+			//position:relative;
+			width: 720px;
+			margin: 0 auto;
+			left: 272px;
+		}
+	`,
+    TopBtn: styled.span`
+		position:fixed;
+		right:24px;
+		bottom:88px;
+		width:40px;
+		height:40px;
+		border:1px solid ${colors.pointBlue};
+		border-radius:22px;
+		background-color:${colors.white};
+		box-shadow:0 4px 10px 0 rgba(0, 0, 0, 0.2);
+		cursor:pointer;
+		svg{
+			position:absolute;
+			top:50%;
+			left:50%;
+			margin-top:-8px;
+			margin-left:-8px;
+		}
+		@media screen and (min-width:768px) {
+			bottom:120px;
+		}
+		@media screen and (min-width:1200px) {
+			z-index:5;
+			right:130px;
+			bottom:80px;
+		}
+	`,
+    ReviewMoreLoading: styled.div`
+        display: block;
+        text-align: center;
+        color: ${colors.pointBlue};
+        padding: 25px
+    `,
+    ScrollView: styled.div``,
+    ReviewPreview: styled.div`
+        @media screen and (min-width:768px) {
+            width: 608px;
+            margin: 0 auto;
+        }
+        
+        @media screen and (min-width: 1200px) {
+            width: 656px;
+            margin: 0 auto;
+            padding-left: 272px;
+        }       
+        img {
+            width: inherit;
+            display: block;
+            margin: 0 auto;
+        }
+    `,
+
+}
 
 const CompanyDetail = () => {
+    const nextPage = useRef(1)
+    const [showScrollView, setShowScrollView] = useState(true)
+
+    const isDesktop = useMedia({
+        minWidth: 1200,
+    })
+    const history = useHistory()
+    const router = useRouter()
+    const params = useParams<{ username: string }>()
+    const dispatch = useDispatch()
+
+    const getPartnerDetail = useSelector(partnerSelector.getPartnerDetail)
+    const getReviewList = useSelector(partnerSelector.getReviewList)
+    const getPartnerPick = useSelector(partnerSelector.getPartnerPick)
+    const getMoveIdxData = useSelector(commonSelector.getMoveIdxData)
+    const getFormData = useSelector(formSelector.getFormData)
+
+    const [sessionVisible, setSessionVisible] = useState(false)
+    const [unavailableCheck, setUnavailableCheck] = useState(false)
+
+    const checkScrollTop = () => {
+        if (!showScrollView && window.pageYOffset > 300) {
+            setShowScrollView(true)
+        } else if (showScrollView && window.pageYOffset <= 300) {
+            setShowScrollView(false)
+        } else if (window.pageYOffset === 0) {
+            setShowScrollView(false)
+        }
+    };
+
+    const handleScrollTop = () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    };
+
+    const handleSelected = () => {
+        if (getPartnerDetail.data) {
+            dispatch(partnerActions.setPartnerPick([getPartnerDetail.data]))
+        }
+
+        router.push('/partner/cart')
+    }
+
+    const isActive = () => {
+        return some(getPartnerPick.data, {
+            adminid: params.username
+        })
+    }
+
+    const buttonText = (status: 'selected' | 'available' | 'unavailable') => {
+        if (status === 'unavailable') {
+            return '오늘 마감된 업체입니다.'
+        }
+
+        if (isActive()) {
+            return '이미 선택된 업체입니다.'
+        }
+
+        return '이 업체에 견적 받아보기'
+    }
+
+    useEffect(() => {
+        if (!getMoveIdxData.idx) {
+            setSessionVisible(true)
+        }
+
+    }, [])
+
+    useEffect(() => {
+        if (getMoveIdxData.idx) {
+            dispatch(partnerActions.fetchPartnerDetailAsync.request({
+                username: params.username,
+                idx: getMoveIdxData.idx
+            }))
+        }
+
+        dispatch(partnerActions.fetchReviewListAsync.request({
+            username: params.username,
+            page: 1,
+            size: values.DEFAULT_REVIEW_LIST_SIZE
+        }))
+    }, [dispatch, params.username])
+
+    useEffect(() => {
+        if (getMoveIdxData.idx && getPartnerDetail?.data?.status === 'unavailable' && !getPartnerDetail.loading && getPartnerDetail?.data?.adminid === params.username) {
+            setUnavailableCheck(true)
+        }
+    }, [getPartnerDetail.loading])
+
+    useEffect(() => {
+        window.addEventListener('scroll', checkScrollTop);
+        return () => {
+            window.removeEventListener('scroll', checkScrollTop);
+        }
+    }, [checkScrollTop])
+
+    if (getPartnerDetail.loading) {
+        return <Loading />
+    }
+
+    const handleMoreReview = () => {
+        nextPage.current += 1;
+        dispatch(partnerActions.fetchReviewMoreListAsync.request({
+            username: params.username,
+            page: nextPage.current,
+            size: values.DEFAULT_REVIEW_LIST_SIZE
+        }))
+    }
 
     return (
-        <>company detail routing</>
+        <S.Container>
+            {getPartnerDetail.data && (
+                <>
+                    {isDesktop ? <MainHeader /> : <TopGnb title="이사업체 상세 정보" count={getPartnerPick.data.length} onPrevious={() => history.goBack()} showTruck={true} />}
+                    <SetType count={getPartnerPick.data.length} formData={getFormData} />
+                    <PartnerInfo title={getPartnerDetail.data.title ? getPartnerDetail.data.title : values.DEFAULT_TEXT} profile_img={getPartnerDetail.data.profile_img} status={getPartnerDetail.data.status}
+                                 level={getPartnerDetail.data.level} pick_cnt={getPartnerDetail.data.pick_cnt} experience={getPartnerDetail.data.experience}
+                                 description={getPartnerDetail.data.description} keywords={getPartnerDetail.data.keywords} adminname={getPartnerDetail.data.adminname} addition={getPartnerDetail.data.addition} />
+                    {/*Review Container*/}
+                    <ReviewContainer />
+                    <S.BottomContainer>
+                        {getReviewList.moreLoading && (
+                            <S.ReviewMoreLoading>
+                                로딩중..
+                            </S.ReviewMoreLoading>
+                        )}
+                        {getReviewList.hasMore && (
+                            <S.MoreList onClick={handleMoreReview}>후기 더보기 <DownArrow width={16} height={16} /></S.MoreList>
+                        )}
+                        {showScrollView && (
+                            <S.ScrollView>
+                                <S.BtnSelect onClick={handleSelected} isSelected={isActive()} status={getPartnerDetail.data.status} id="dsl_booking_detail_cta">{buttonText(getPartnerDetail.data.status)}</S.BtnSelect>
+                                <S.TopBtn onClick={handleScrollTop}>
+                                    <UpArrow color={colors.pointBlue} width={16} height={16} />
+                                </S.TopBtn>
+                            </S.ScrollView>
+                        )}
+                    </S.BottomContainer>
+                </>
+            )}
+            <ToastPopup visible={sessionVisible} confirmText={'홈으로 가기'} confirmClick={() => history.push('/')} showHeaderCancelButton={false}>
+                <p>{'정보가 만료되었습니다.\n다시 조회해주세요'}</p>
+            </ToastPopup>
+            <ToastPopup visible={unavailableCheck} showHeaderCancelButton={false} confirmClick={() => setUnavailableCheck(!unavailableCheck)} confirmText={"확인"}>
+                <p>오늘 마감된 업체</p>
+                <span>해당 업체는 오늘 예약 및 상담 접수가 마감됐어요. 내일 오전에 다시 조회해보세요!</span>
+            </ToastPopup>
+        </S.Container>
     )
 }
 
