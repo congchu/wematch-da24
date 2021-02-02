736c2a6ca05faf01298890b307d066d61b3daea5
Chanhee Jeong <chan420420@gmail.com>
2021-02-02 18:43:10 +0900

    collaps 완성 (별도의 애니메이션 없음)

--- src/pages/faq/collapsed.tsx
+++ src/pages/faq/collapsed.tsx
@@ -17,13 +17,14 @@ interface Props extends React.HTMLAttributes<HTMLDivElement> {
 const S = {
     TopTitleWrap: styled.div`
       overflow: hidden;
-      font-size: 14px;
       line-height: 1.5;
       color: #333;
       font-weight: 600;
       padding: 0px 64px 16px 24px;
       background-size: 16px;
       cursor: pointer;
+      font-size: 16px;
+      letter-spacing: -1px;
 
       svg {
         float: right;
@@ -47,30 +48,34 @@ const S = {
     `,
     TitleWrap: styled.div`
       overflow: hidden;
-      font-size: 14px;
       line-height: 1.5;
       color: #333;
       font-weight: 600;
-      padding: 0px 64px 16px 24px;
+      font-size: 16px;
+      letter-spacing: -1px;
+      padding: 0px 64px 0px 24px;
       background-size: 16px;
       cursor: pointer;
+      position: relative;
+      
+      .textWrapper {
+        display: inline-block;
+        em {
+          color: #1672f7;
+        }
+      }
+      
+      span {
+        display: inline;
+        position: absolute;
+        top: 25%;
+        right: 24px;
+      }
 
       svg {
         float: right;
       }
-
-      em {
-        font-size: 16px;
-        color: #1672f7;
-        letter-spacing: -1px;
-
-      }
-
-      span {
-        font-size: 16px;
-        letter-spacing: -1px;
-      }
-
+      
       @media screen and (min-width: 768px) {
         //padding-top: 52px;
       }
@@ -100,12 +105,14 @@ export default function Collapsed({ category ,title, children, defaultExpand }: 
 
     return(
         <>
-            <S.TopTitleWrap onClick={() => setExpand(!expand)} className="toggle">
-                <em>Q{category}</em>
-                <br/>
-                <span>{title}</span>
-                {expand ? <Plus style={{marginTop: 0}}/> : <Minus style={{marginTop: 0}} color={colors.pointBlue}/>}
-            </S.TopTitleWrap>
+            <S.TitleWrap onClick={() => setExpand(!expand)} className="toggle">
+                <div className='textWrapper'>
+                    <em>Q{category}</em>
+                    <br/>
+                    {title}
+                </div>
+                <span>{expand ? <Plus style={{marginTop: 0}}/> : <Minus style={{marginTop: 0}} color={colors.pointBlue}/>}</span>
+            </S.TitleWrap>
             <S.AnswerWrap>
                 <S.Container expand={expand}>
                     {children}
--- src/pages/faq/index.tsx
+++ src/pages/faq/index.tsx
@@ -32,6 +32,10 @@ const S = {
         //padding-top: 52px;
       }
     `,
+    CollapsedWrap: styled.div `
+      padding-top: 18px;
+      border-bottom: 1px solid #d7dbe2;
+    `
 }
 
 export default function FaqPage() {
@@ -42,14 +46,26 @@ export default function FaqPage() {
     return(
         <Layout title="자주 묻는 질문">
             {/*이렇게 나중에 나누기*/}
-            <Collapsed category='공통' title='소비자평가등급은 무엇인가요?' defaultExpand={true}>
-                이사/청소 업체의 서비스 품질을 가장 정확하게 예측할 수 있는 객관적인 지표입니다. <br/>
-                소고기, 호텔 등을 고를 때 공인된 등급을 확인하듯, 이사/청소 업체 선택 시 가장 합리적인 기준이 됩니다. <br/>
-                <br/>
-                소비자평가등급에 대해 보다 자세한 내용은 아래 링크에서 확인해주세요.<br/>
-                <br/>
-                <a href="https://da24.wematch.com/da24.asp">바로가기 >></a>
-            </Collapsed>
+            <S.CollapsedWrap>
+                <Collapsed category='공통' title='소비자평가등급은 무엇인가요?' defaultExpand={false}>
+                    이사/청소 업체의 서비스 품질을 가장 정확하게 예측할 수 있는 객관적인 지표입니다. <br/>
+                    소고기, 호텔 등을 고를 때 공인된 등급을 확인하듯, 이사/청소 업체 선택 시 가장 합리적인 기준이 됩니다. <br/>
+                    <br/>
+                    소비자평가등급에 대해 보다 자세한 내용은 아래 링크에서 확인해주세요.<br/>
+                    <br/>
+                    <a href="https://da24.wematch.com/da24.asp">바로가기 >></a>
+                </Collapsed>
+            </S.CollapsedWrap>
+            <S.CollapsedWrap>
+                <Collapsed category='공통' title='소비자평가등급은 무엇인가요?' defaultExpand={false}>
+                    이사/청소 업체의 서비스 품질을 가장 정확하게 예측할 수 있는 객관적인 지표입니다. <br/>
+                    소고기, 호텔 등을 고를 때 공인된 등급을 확인하듯, 이사/청소 업체 선택 시 가장 합리적인 기준이 됩니다. <br/>
+                    <br/>
+                    소비자평가등급에 대해 보다 자세한 내용은 아래 링크에서 확인해주세요.<br/>
+                    <br/>
+                    <a href="https://da24.wematch.com/da24.asp">바로가기 >></a>
+                </Collapsed>
+            </S.CollapsedWrap>
         </Layout>
     )
 
