cd65ac707122cd204733c92cf16cb06bfcb7012e
Chanhee Jeong <chan420420@gmail.com>
2021-02-03 16:55:55 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,6 @@ import store from 'store/index'
 import browserHistory from 'lib/history'
 import GlobalStyled from 'styles/global'
 
-import faq from 'pages/faq'
 import Home from 'pages/home'
 import PartnerList from 'pages/partner/List/index'
 import PartnerDetail from 'pages/partner/Detail/index'
@@ -99,7 +98,7 @@ function AppRoute() {
     } else {
         return (
             <Switch>
-                <Route exact path="/" component={faq}/>
+                <Route exact path="/" component={Home}/>
                 <Route exact path="/partner/list" component={PartnerList}/>
                 <Route exact path="/partner/detail/:adminId" component={PartnerDetail}/>
                 <Route exact path="/partner/cart" component={PartnerCart}/>
