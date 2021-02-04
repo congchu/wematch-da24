5cf44177e76850c6cbf93a5406ba571a16d68781
Chanhee Jeong <chan420420@gmail.com>
2021-02-04 16:29:19 +0900

    체크리스트 디자인완성 (w/임시아이콘)

---
+++ src/pages/checklist/components/cleanEstimation.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled, { css } from 'styled-components'
+import Layout from 'components/base/Layout'
+
+
+const S = {
+
+}
+
+
+
+function cleanEstimation() {
+    return (
+        <Layout title='체크리스트' subTitle={<>이사/청소 준비 어려우셨죠?<br/>체크리스트만 따라해보세요!</>}>
+
+        </Layout>
+    )
+
+}
+
+export default cleanEstimation
---
+++ src/pages/checklist/components/moveEstimation.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import styled, { css } from 'styled-components'
+import Layout from 'components/base/Layout'
+
+
+const S = {
+
+}
+
+
+
+function MoveEstimation() {
+    return (
+        <Layout title='체크리스트' subTitle={<>이사/청소 준비 어려우셨죠?<br/>체크리스트만 따라해보세요!</>}>
+
+        </Layout>
+    )
+
+}
+
+export default MoveEstimation
+
+
