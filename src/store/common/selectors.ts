d48e737322e49d4481c65b9deca56f3e6921606a
Yesdoing <fkrmtleo12@gmail.com>
2021-03-02 15:12:33 +0900

    [feature] device id common 스토어 추가

--- src/store/common/selectors.ts
+++ src/store/common/selectors.ts
@@ -5,3 +5,4 @@ const getCommonState = (state: RootState) => state.commonState
 
 export const getAddressList = createSelector(getCommonState, state => state.addressList);
 export const getMoveIdxData = createSelector(getCommonState, state => state.moveIdxData);
+export const getDeviceId = createSelector(getCommonState, state => state.deviceId);
