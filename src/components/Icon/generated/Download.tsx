a641d9bf5e7410e5b1460fd99a0f12d0deee4caa
Chanhee Jeong <chan420420@gmail.com>
2021-03-09 15:27:26 +0900

    Download SVG : path 수정

--- src/components/Icon/generated/Download.tsx
+++ src/components/Icon/generated/Download.tsx
@@ -18,9 +18,9 @@ export default class SvgDownArrow extends React.PureComponent<IconProps> {
   public render() {
     return(
         <svg width={this.props.width} height={this.props.height} style={this.props.style} className={this.props.className}  viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M4 7L8 10L12 7" stroke={this.props.color} stroke-linecap="round"/>
-          <path d="M2 9V14H14V9" stroke={this.props.color} stroke-linecap="round" stroke-linejoin="round"/>
-          <path d="M8 10V2" stroke={this.props.color} stroke-linecap="round"/>
+          <path d="M4 7L8 10L12 7" stroke={this.props.color} strokeLinecap="round"/>
+          <path d="M2 9V14H14V9" stroke={this.props.color} strokeLinecap="round" strokeLinejoin="round"/>
+          <path d="M8 10V2" stroke={this.props.color} strokeLinecap="round"/>
         </svg>
         )
 
