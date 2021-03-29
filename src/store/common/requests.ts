dae39a7bf389d3527906a617ede03e2207ea5319
Yesdoing <fkrmtleo12@gmail.com>
2021-03-29 09:57:45 +0900

    [feature] 사용하지 않는 api 삭제

--- src/store/common/requests.ts
+++ src/store/common/requests.ts
@@ -9,28 +9,6 @@ export const getAddress = async (dong: string) => {
     return data
 }
 
-export const verifySendMessage = async (phone: string) => {
-    const { data } = await api.request<types.RequestVerifySendMessageProps>({
-        method: 'post',
-        url: `/msg/auth`,
-        data: {
-            'send_phone': 'DA24_CUSTOMER',
-            'dest_phone': `${phone}`
-        }
-    })
-
-    return data
-}
-
-export const verifyAuthCode = async (phone: string, code: string) => {
-    const { data } = await api.request<types.RequestVerifyAuthCodeProps>({
-        method: 'get',
-        url: `${API_MESSAGE_URL}?dest_phone=${phone}&code=${code}`
-    })
-
-    return data
-}
-
 export const getMoveIdx = async (formData: types.RequestUserInfoInsert) => {
     return await axios.post(`${API_MIDDLEWARE_URL}/inquiry`, formData)
 }
