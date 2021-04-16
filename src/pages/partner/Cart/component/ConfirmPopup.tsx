4d1e5d489ff7b876fb284194359a9d3d9f855d7e
marcus <rhcn258@gmail.com>
2021-04-16 15:29:47 +0900

    직접고르기 모달 수정

--- src/pages/partner/Cart/component/ConfirmPopup.tsx
+++ src/pages/partner/Cart/component/ConfirmPopup.tsx
@@ -12,7 +12,7 @@ interface Props {
 const ConfirmPopup:React.FC<Props> = (props) => {
     const { visible, showHeaderCancelButton, orderCount, confirmClick, cancelClick} = props
     return (
-        <NewModal visible={visible} title={"업체 선"} content={"업체는 최대 3개까지 선택하실 수 있으며 그 이상의 이사업체 비교를 원하시면 고객센터(1522-2483)로 문의해주세요."} confirmClick={cancelClick} confirmText={"확인"}/>
+        <NewModal visible={visible} title={"업체 선택"} content={"선택하신 3개 업체에 방문견적 요청을 보내겠습니다."} confirmClick={confirmClick} confirmText={"네 보내주세요."}/>
         /*<ToastPopup visible={visible} showHeaderCancelButton={showHeaderCancelButton} confirmText={'네, 보내주세요'} confirmClick={confirmClick} closeClick={cancelClick}>
             <h1>{`선택하신 ${orderCount}개 업체에 방문견적 요청을 보내시겠습니까?`}</h1>
         </ToastPopup>*/
