a391813704993a2e476b96f9a086ace9bf1338d7
Yesdoing <fkrmtleo12@gmail.com>
2021-05-13 10:57:40 +0900

    [refactor] change Input to InputBox Component

---
+++ src/components/InputBox/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Icon } from 'components/wematch-ui'
+import * as colors from 'styles/colors'
+
+const InputWrapper = styled.div<{ error: boolean }>`
+  display: flex;
+  align-items: center;
+  padding: 16px;
+  box-sizing: border-box;
+  width: 100%;
+  min-height: 56px;
+  /* height: 56px; */
+  border: ${({ error }) => (error ? '1px solid #EC485C' : '1px solid #d7dbe2')};
+  border-radius: 6px;
+
+  span {
+    font-size: 16px;
+    letter-spacing: -1px;
+    color: ${colors.gray88};
+  }
+`
+
+interface IProps {
+  icon?: string
+  placeHolder?: string
+  value?: string
+  onClick: () => void
+  style?: React.CSSProperties
+  error?: boolean
+}
+
+const InputBox: React.FC<IProps> = ({ icon, placeHolder, value, onClick, style, error = false }) => {
+  return (
+    <InputWrapper onClick={onClick} style={style} error={error}>
+      <div style={{ flex: 1, wordBreak: 'keep-all' }}>{!value ? <span>{placeHolder}</span> : <span>{value}</span>}</div>
+      <div style={{ width: '22px', height: '22px', marginLeft: '8px' }}>
+        {icon === 'search' && <Icon.Search size={22} />}
+        {icon === 'down' && <Icon.Down size={22} />}
+      </div>
+    </InputWrapper>
+  )
+}
+
+export default InputBox
--- src/pages/home/components/MoveInput/index.tsx
+++ src/pages/home/components/MoveInput/index.tsx
@@ -22,6 +22,9 @@ import * as formActions from 'store/form/actions'
 import { isExceedDiffDay } from 'lib/dateUtil'
 import { dataLayer } from 'lib/dataLayerUtil'
 import * as colors from 'styles/colors'
+import { Icon } from 'components/wematch-ui'
+import InputBox from 'components/InputBox'
+import { EFormError } from '../MoveForm'
 
 type MoveInputProps = {
   departAddress: string
@@ -35,6 +38,7 @@ type MoveInputProps = {
 interface Props extends Omit<React.HTMLAttributes<HTMLElement>, 'onChange'> {
   type: formActions.MoveTypeProp
   onChange?(estimate: MoveInputProps): void
+  formValidations: EFormError[]
 }
 
 const S = {
@@ -83,7 +87,7 @@ const S = {
 }
 
 const MoveInput: React.FC<Props> = (props) => {
-  const { type, onChange, ...restProps } = props
+  const { type, onChange, formValidations, ...restProps } = props
 
   const dispatch = useDispatch()
 
@@ -91,8 +95,6 @@ const MoveInput: React.FC<Props> = (props) => {
   const getMoveDate = useSelector(formSelector.getDate)
   const getMoveAddress = useSelector(formSelector.getAddress)
   const getMoveFloor = useSelector(formSelector.getFloor)
-  const getName = useSelector(formSelector.getName)
-  const getPhone = useSelector(formSelector.getPhone)
   const getJuso = useSelector(commonSelector.getJuso)
 
   const floorItems = [
@@ -123,12 +125,6 @@ const MoveInput: React.FC<Props> = (props) => {
     { key: '25', value: '25층 이상' }
   ]
 
-  // const [visibleCalendarModal, setVisibleCalendarModal] = useState<boolean>(false)
-  // const [visibleStartAddressModal, setVisibleStartAddressModal] = useState<boolean>(false)
-  // const [visibleStartFloorModal, setVisibleStartFloorModal] = useState<boolean>(false)
-  // const [visibleEndAddressModal, setVisibleEndAddressModal] = useState<boolean>(false)
-  // const [visibleEndFloorModal, setVisibleEndFloorModal] = useState<boolean>(false)
-
   const [visibleCalendarModal, setVisibleCalendarModal] = useHashToggle('#calendar')
   const [visibleStartAddressModal, setVisibleStartAddressModal] = useHashToggle('#startAddress')
   const [visibleStartFloorModal, setVisibleStartFloorModal] = useHashToggle('#startFloor')
@@ -146,25 +142,11 @@ const MoveInput: React.FC<Props> = (props) => {
   const toggleCalendarCancel = () => {
     setVisibleCalendarModal(!visibleCalendarModal)
   }
-  const toggleCalendarConfirm = () => {
-    dataLayer({
-      event: 'input_info',
-      category: '다이사_메인_입력창_1',
-      label: getMoveDate[0],
-      action: '이사날짜',
-      CD6: getMoveTypeText()
-    })
-    setVisibleCalendarModal(!visibleCalendarModal)
-  }
+
   const toggleStartAddress = () => setVisibleStartAddressModal(!visibleStartAddressModal)
   const toggleStartFloor = () => setVisibleStartFloorModal(!visibleStartFloorModal)
   const toggleEndAddress = () => setVisibleEndAddressModal(!visibleEndAddressModal)
   const toggleEndFloor = () => setVisibleEndFloorModal(!visibleEndFloorModal)
-
-  const handlePhone = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const originPhoneValue = event.target.value.replace(/-/gi, '')
-    dispatch(formActions.setPhone(originPhoneValue))
-  }
 
   const onSelectDate = (date: CalendarDate) => {
     if (isExceedDiffDay(date, CALENDAR_MAX_DAYS)) {
@@ -282,36 +264,48 @@ const MoveInput: React.FC<Props> = (props) => {
     })
   }
 
+  const renderFloor = useCallback(
+    (type: 'start' | 'end') => {
+      return getMoveFloor[type] ? (getMoveFloor[type] === '25' ? `${getMoveFloor[type]}층 이상` : `${getMoveFloor[type]}층`) : getMoveFloor[type]
+    },
+    [getMoveFloor]
+  )
+
+  const renderAddressValue = useCallback((type: 'start' | 'end') => (getJuso.type?.start === 'road' ? getJuso[type]?.roadAddr : getJuso[type]?.jibunAddr), [getJuso])
+
   return (
     <S.Container id="dsl_move_input_terms_1" {...restProps} type={type}>
       <S.Form>
         <S.Title>이사 날짜</S.Title>
-        <Input theme="default" border readOnly placeholder="이사예정일" onClick={() => setVisibleCalendarModal(true)} value={getMoveDate} style={{ backgroundColor: 'transparent' }} icon="down" />
+        <InputBox icon={'down'} placeHolder={'날짜 선택'} value={getMoveDate.join()} onClick={() => setVisibleCalendarModal(true)} error={formValidations.includes(EFormError.DATE)} />
+        {/* <Input theme="default" border readOnly placeholder="이사예정일" onClick={() => setVisibleCalendarModal(true)} value={getMoveDate} style={{ backgroundColor: 'transparent' }} icon="down" /> */}
         <div style={{ display: 'flex', flexDirection: 'column', marginTop: '24px' }}>
           <S.Title>출발지 주소</S.Title>
-          <Input theme="default" border readOnly placeholder="출발지" rootStyle={{ width: '100%' }} onClick={toggleStartAddress} value={getJuso.type?.start === 'road' ? getJuso.start?.roadAddr : getJuso.start?.jibunAddr} style={{ backgroundColor: 'transparent' }} icon="search" />
-          <Input theme="default" border readOnly icon="down" placeholder="층수" rootStyle={{ width: '100%' }} onClick={toggleStartFloor} value={getMoveFloor.start ? getMoveFloor.start + '층' : getMoveFloor.start} style={{ backgroundColor: 'transparent' }} />
+          <InputBox icon={'search'} placeHolder={'주소 검색'} value={renderAddressValue('start')} onClick={toggleStartAddress} style={{ marginBottom: '8px' }} error={formValidations.includes(EFormError.START_ADDRESS)} />
+          {/* <Input theme="default" border readOnly placeholder="출발지" rootStyle={{ width: '100%' }} onClick={toggleStartAddress} value={getJuso.type?.start === 'road' ? getJuso.start?.roadAddr : getJuso.start?.jibunAddr} style={{ backgroundColor: 'transparent' }} icon="search" /> */}
+          <InputBox icon={'down'} placeHolder={'층수 검색'} value={renderFloor('start')} onClick={toggleStartFloor} error={formValidations.includes(EFormError.START_FLOOR)} />
+          {/* <Input theme="default" border readOnly icon="down" placeholder="층수 선택" rootStyle={{ width: '100%' }} onClick={toggleStartFloor} value={renderFloor('start')} style={{ backgroundColor: 'transparent' }} /> */}
         </div>
-        {getMoveAddress?.start && (
-          <div style={{ display: 'flex', flexDirection: 'column', marginTop: '24px' }}>
-            <S.Title>도착지 주소</S.Title>
-            <Input theme="default" border readOnly placeholder="도착지" rootStyle={{ width: '100%' }} onClick={toggleEndAddress} value={getJuso.type?.end === 'road' ? getJuso.end?.roadAddr : getJuso.end?.jibunAddr} style={{ backgroundColor: 'transparent' }} icon="search" />
-            <Input theme="default" border readOnly icon="down" placeholder="층수" rootStyle={{ width: '100%' }} onClick={toggleEndFloor} value={getMoveFloor.end ? getMoveFloor.end + '층' : getMoveFloor.end} style={{ backgroundColor: 'transparent' }} />
-          </div>
-        )}
-        {getMoveAddress?.end && (
-          <div style={{ display: 'flex', flexDirection: 'column', marginTop: '24px' }}>
-            <S.Title>업체 전달 메모(선택)</S.Title>
-            <S.TextContainer>
-              <S.Textarea
-                placeholder="업체에게 전달할 메모가 있다면 작성해 주세요."
-                onChange={(e) => {
-                  dispatch(formActions.setContents(e.target.value))
-                }}
-              />
-            </S.TextContainer>
-          </div>
-        )}
+
+        <div style={{ display: 'flex', flexDirection: 'column', marginTop: '24px' }}>
+          <S.Title>도착지 주소</S.Title>
+          <InputBox icon={'search'} placeHolder={'주소 검색'} value={renderAddressValue('end')} onClick={toggleEndAddress} style={{ marginBottom: '8px' }} error={formValidations.includes(EFormError.END_ADDRESS)} />
+          {/* <Input theme="default" border readOnly placeholder="도착지" rootStyle={{ width: '100%' }} onClick={toggleEndAddress} value={getJuso.type?.end === 'road' ? getJuso.end?.roadAddr : getJuso.end?.jibunAddr} style={{ backgroundColor: 'transparent' }} icon="search" /> */}
+          <InputBox icon={'down'} placeHolder={'층수 검색'} value={renderFloor('end')} onClick={toggleEndFloor} error={formValidations.includes(EFormError.END_FLOOR)} />
+          {/* <Input theme="default" border readOnly icon="down" placeholder="층수" rootStyle={{ width: '100%' }} onClick={toggleEndFloor} value={getMoveFloor.end ? getMoveFloor.end + '층' : getMoveFloor.end} style={{ backgroundColor: 'transparent' }} /> */}
+        </div>
+
+        <div style={{ display: 'flex', flexDirection: 'column', marginTop: '24px' }}>
+          <S.Title>업체 전달 메모(선택)</S.Title>
+          <S.TextContainer>
+            <S.Textarea
+              placeholder="업체에게 전달할 메모가 있다면 작성해 주세요."
+              onChange={(e) => {
+                dispatch(formActions.setContents(e.target.value))
+              }}
+            />
+          </S.TextContainer>
+        </div>
       </S.Form>
       <CalendarModal visible={visibleCalendarModal} title="이사 예정일이 언제세요?" onClose={toggleCalendarCancel} onSelect={onSelectDate} selected={getMoveDate} />
       <AddressModal visible={visibleStartAddressModal} title="출발지를 검색해주세요" onClose={toggleStartAddress} onConfirm={toggleStartAddress} onClick={toggleStartAddress} onSelect={onSelectStartAddress} />
