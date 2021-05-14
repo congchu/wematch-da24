1933aee54d7f2290e5513c17e4003ddaff77ba1a
yesdoing <fkrmtleo12@gmail.com>
2021-05-14 16:49:36 +0900

    [bugfix] change import position

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,15 +1,15 @@
-import {createGlobalStyle} from "styled-components";
-import reset from "./reset";
-import {resetButton, resetInput} from "./mixins";
-import * as colors from "styles/colors";
+import { createGlobalStyle } from 'styled-components'
+import reset from './reset'
+import { resetButton, resetInput } from './mixins'
+import * as colors from 'styles/colors'
 
 export default createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700&display=swap');
   ${reset}
   html, body, #root {
     height: 100%;
   }
   body {
-    @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700&display=swap');
     min-width:320px;
     font-family: 'Noto Sans KR', 'AppleSDGothicNeo-Light', 'Malgun Gothic', '맑은 고딕', sans-serif;
     font-size: 14px;
@@ -33,4 +33,4 @@ export default createGlobalStyle`
   input {
     ${resetInput};
   }
-`;
+`
