c1dca42829786fb9fc9e7efccd1389bbafb1364e
KooMinJeong <cookie00421@gmail.com>
2021-10-21 13:25:36 +0900

    [fix] 날짜 선택 모달 하단 텍스트 수정

--- src/components/common/DatePicker/index.tsx
+++ src/components/common/DatePicker/index.tsx
@@ -179,7 +179,7 @@ const DatePicker: React.FC<Props> = (props) => {
           <p className="title">
             <em>견적신청 가능 날짜</em>
           </p>
-          <p className="content">{serviceType === 'move' ? `55일 이내 날짜에서만 이사업체 견적신청이 가능해요. (당일, 익일 이사 불가)` : `오늘부터 55일 이내 날짜에서만 청소업체 견적신청이 가능해요.`}</p>
+          <p className="content">{serviceType === 'move' ? `55일 이내 날짜에서만 이사업체 견적신청이 가능해요. (당일, 익일 이사 불가)` : `내일부터 55일 이내 날짜에서만 청소업체 견적신청이 가능해요.`}</p>
         </S.Info>
         {serviceType === 'move' && (
           <S.Toast>
