e750b4fdc57867548c264319e7846d172b3874b9
congchu <cookie00421@gmail.com>
2021-12-03 16:44:28 +0900

    [fix] 스피너, 스피너 모달 UI 업데이트

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -257,7 +257,7 @@ function LoginPage() {
             <span onClick={() => setVisibleTerms(true)}>이용약관 및 개인정보처리방침 동의</span>, 견적상담을 위한 개인 정보 제3자 제공 및 마케팅 정보수신 동의 필요
           </p>
           <Button theme="primary" disabled={!isVerified || !name} style={{ fontSize: "18px", borderRadius: "6px" }} bold={true} onClick={handleSignUp}>
-            {!submitted ? "동의하고 진행하기" : <Spinner color="white" />}
+            {!submitted ? "동의하고 진행하기" : <Spinner color="white" size="small" />}
           </Button>
           {isDesktop && (
             <Button theme="default" border={true} style={{ border: "1px solid #D7DBE2", marginTop: 10 }} onClick={() => setVisibleCancel(true)}>
